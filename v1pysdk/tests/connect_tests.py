3fa76a08eb312479e3a39c5b38f590c297014b67
Joe Koberg <joe.koberg@versionone.com>
2012-05-10 12:02:50 -0400

    fixed call interface to client

--- v1pysdk/tests/connect_tests.py
+++ v1pysdk/tests/connect_tests.py
@@ -1,22 +1,16 @@
 from testtools import TestCase
 from testtools.matchers import Equals
 
-
 from elementtree.ElementTree import parse, fromstring, ElementTree
-
-
 
 from v1pysdk.client import *
 
-
 class TestV1Connection(TestCase):
-  
   def test_connect(self, username='admin', password='admin'):
-    
     server = V1Server(username=username, password=password)
-    
-    code, body = server.connect('rest-1.v1/Data/Story')
-
+    code, body = server.get('/rest-1.v1/Data/Story')
+    print "\n\nCode: ", code
+    print "Body: ", body
     elem = fromstring(body)
     self.assertThat(elem.tag, Equals('Assets'))
 
