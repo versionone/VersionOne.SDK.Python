91498b6b55fc87f9b9f4d9815aacd81c34a49eb7
Joe Koberg <joe.koberg@versionone.com>
2012-05-10 13:45:52 -0400

    Changed to global cache mainly operated by the BaseAsset class

--- v1pysdk/v1meta.py
+++ v1pysdk/v1meta.py
@@ -17,18 +17,30 @@ the cached moment, or else you lost.
 
 
 class BaseAsset(object):
+  """
+     Provides common methods for the dynamically derived asset type classes
+     built by V1Meta.create_asset_proxy_class
+  """
+
   @classmethod
-  def find(Class, query):
-    results = Class._v1_v1meta_instance.run_query(query)
-    return [Class(result) for result in results]
-    
+  def find_by_id(Class, asset_id):
+    cache_key = (Class._v1_asset_type_name, asset_id)
+    cache = Class._v1_v1meta_instance.global_cache
+    if cache.has_key(cache_key):
+      return cache[cache_key]
+    new_asset_instance = Class(asset_id)
+    cache[cache_key] = new_asset_instance
+    return new_asset_instance
+
   def __init__(self, oid):
     self._v1_oid = oid
     self._v1_new_data = {}
     self._v1_current_data = {}
-    self._v1_refresh()
+    self._v1_needs_refresh = True
 
   def __getattr__(self, attr):
+    if self._v1_needs_refresh:
+      self._v1_refresh()
     if self._v1_new_data.has_key(attr):
       return self._v1_new_data[attr]
     return self._v1_current_data[attr]
@@ -38,16 +50,16 @@ class BaseAsset(object):
       object.__setattr__(self, attr, value)
     else:
       self._v1_new_data[attr] = value
-      self._v1_dirty = True
+      self._v1_needs_commit = True
      
   def _v1_commit(self):
-    if self._v1_dirty:
-      self._v1_v1_meta_instance.update_asset(self._v1_asset_type_name, self._v1_oid, self._v1_new_data)
-    self._v1_refresh()
+    if self._v1_needs_commit:
+      self._v1_v1meta_instance.update_asset(self._v1_asset_type_name, self._v1_oid, self._v1_new_data)
+    self._v1_needs_refresh = True
     
   def _v1_refresh(self):
     self._v1_current_data = self._v1_v1meta_instance.read_asset(self._v1_asset_type_name, self._v1_oid)
-    self._v1_dirty = False
+    self._v1_needs_refresh = False
 
 
     
@@ -78,6 +90,7 @@ def cached_by_keyfunc(keyfunc):
 class V1Meta(object):
   def __init__(self, username='admin', password='admin'):
     self.server = V1Server(username=username, password=password)
+    self.global_cache = {}
     
   def make_moment_keyfunc_for_asset_type(asset_type_name):
     def keyfunc(old_f, args, kw, data):
@@ -110,30 +123,24 @@ class V1Meta(object):
   def update_asset(self, asset_type_name, asset_oid, newdata):
     raise NotImplemented
     
-  def read_asset(self, asset_type_name, asset_oid, oids_read=None):
-    if oids_read is None:
-      oids_read = {}
-      
+  def read_asset(self, asset_type_name, asset_oid):
     xml = self.server.get_asset_xml(asset_type_name, asset_oid)
-    
     output = {}
     
     for attribute in xml.findall('Attribute'):
       key = attribute.get('name').replace('.','_')
       value = attribute.text
       output[key] = value
-    
-    oids_read[(asset_type_name, asset_oid)] = output
-      
+
     for relation in xml.findall('Relation'):
       key = relation.get('name')
-      for value_element in relation.findall('Asset'):
+      related_asset_elements = relation.findall('Asset')
+      output[key] = None
+      for value_element in related_asset_elements:
         relation_idref = value_element.get('idref')
         reltype, relid = relation_idref.split(':')
-        if (reltype, relid) in oids_read:
-          value = oids_read[(reltype, relid)]
-        else:
-          value = self.read_asset(reltype, relid, oids_read=oids_read)
+        valueclass = self.create_asset_proxy_class(reltype, None)
+        value = valueclass.find_by_id(relid)        
         output[key] = value
         
     return output
