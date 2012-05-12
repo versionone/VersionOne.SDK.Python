40341df09f8a29feff419b55d9aa239ed57021e2
Joe Koberg <joe.koberg@versionone.com>
2012-05-12 12:10:38 -0400

    made speed example actually load the objects

--- examples/speed_example.py
+++ examples/speed_example.py
@@ -9,9 +9,17 @@ t0 = time.time()
 
 all_asset_types = meta.AssetType.query()
 all_queries = [meta.asset_class(type.Name).query() for type in all_asset_types] 
-all_assets = [asset
-              for query in all_queries
-              for asset in query ]
+
+def process_queries(queries):
+  for query in queries:
+    for asset in query:
+        try:
+          asset._v1_refresh()
+          yield str(asset)
+        except Exception, e:
+          yield 'Error! %s(%s)'%(asset._v1_asset_type_name, asset._v1_oid)
+
+all_assets = list(process_queries(all_queries))
 
 t1 = time.time()
 
@@ -20,5 +28,6 @@ count = len(all_assets)
 
 print "%d assets in %0.4fs (%0.4fs/asset)"%(count, elapsed, elapsed/count)
 
+out = open('output.txt', 'w').write('\n'.join([str(a) for a in all_assets]))
 
 
