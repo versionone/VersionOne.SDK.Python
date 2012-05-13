5532da0d84d2e8b66b95fc41081c92dee6927cca
Joe Koberg <joe.koberg@versionone.com>
2012-05-13 19:41:51 -0400

    cleanup example

--- examples/creation_example.py
+++ examples/creation_example.py
@@ -4,9 +4,13 @@ from v1pysdk import V1Meta
 
 v1 = V1Meta()
 
-new_story = v1.Story.create(Name="New Story", Scope=v1.Scope(1002))
+new_story = v1.Story.create(
+                      Name = "New Story", 
+                      Scope = v1.Scope(1002),
+                      )
 
-new_story.Owners = [v1.Member(20)]
+
+new_story.Owners = list(v1.Member.where(Name="Admin"))
 
 v1.commit()
 
