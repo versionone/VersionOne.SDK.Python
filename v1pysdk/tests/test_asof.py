1f34488ffcb3debe74f74de5f64038a87467cfc3
Joe Koberg <joe.koberg@versionone.com>
2012-10-11 12:52:06 -0400

    Use V1Meta as a context manager and condense whitespace

--- v1pysdk/tests/test_asof.py
+++ v1pysdk/tests/test_asof.py
@@ -2,7 +2,6 @@ import sys, datetime, csv, os, pprint
 
 from v1pysdk import V1Meta
 
-
 statuses = [
     'Not Started',
     'Ready for Dev',
@@ -17,7 +16,7 @@ select_template = "Workitems:PrimaryWorkitem[Status.Name='{0}'].Estimate.@Sum"
 def parsedate(d):
   return datetime.datetime.strptime(d, "%Y-%m-%d")
 
-def as_of_times(start, end, hoursper=8):
+def as_of_times(start, end, hoursper=6):
   current = start
   while current <= end:
     yield current
@@ -25,35 +24,25 @@ def as_of_times(start, end, hoursper=8):
 
 if __name__=="__main__":
     username, password, sprintName, outputFolder = sys.argv[1:5]
-
-    v1 = V1Meta('www7.v1host.com', 'V1Production', username, password)
-
-    timebox = (v1.Timebox
-                 .where(Name=sprintName)
-                 .select("BeginDate", "EndDate")
-                 .first()
-              )
-    
-    startdate = parsedate(timebox.BeginDate)
-    enddate = parsedate(timebox.EndDate)
-    individual_times = as_of_times(startdate, enddate)
-    
-    select_list = [ select_template.format(status) for status in statuses ]
-    
-    results = (
-        v1.Timebox
-          .asof(individual_times)
-          .where(Name=sprintName)
-          .select(*select_list)
-        )
-          
-    outfilename = os.path.join(outputFolder,  sprintName + ".dat")
-    
-    with open(outfilename, "w") as outfile:
-        writer = csv.writer(outfile, delimiter="|")
-        writer.writerow(['# Date'] + statuses)
-        for result in results:
-          row = [result.data[select_term] for select_term in select_list]
-          writer.writerow([result.data['AsOf']] + row)
-    
+    with V1Meta('www7.v1host.com', 'V1Production', username, password) as v1:
+        timebox = (v1.Timebox
+                     .where(Name=sprintName)
+                     .select("BeginDate", "EndDate")
+                     .first())
+        startdate = parsedate(timebox.BeginDate)
+        enddate = parsedate(timebox.EndDate) + datetime.timedelta(days=1)
+        individual_times = as_of_times(startdate, enddate)
+        select_list = [ select_template.format(status) for status in statuses]
+        results = (v1.Timebox
+                     .asof(individual_times)
+                     .where(Name=sprintName)
+                     .select(*select_list))
+        outfilename = os.path.join(outputFolder,  sprintName + ".dat")
+        with open(outfilename, "w") as outfile:
+            writer = csv.writer(outfile, delimiter="|")
+            writer.writerow(['# Date'] + statuses)
+            for result in results:
+              row = [result.data[select_term] for select_term in select_list]
+              writer.writerow([result.data['AsOf']] + row)
+        
 
