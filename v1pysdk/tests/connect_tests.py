0424ae096d4fa87ad5bbf9859455441652f7ad33
kunzimariano <kunzi.mariano@gmail.com>
2014-08-06 12:07:14 -0300

    Made connect_test.py work.

--- v1pysdk/tests/connect_tests.py
+++ v1pysdk/tests/connect_tests.py
@@ -7,14 +7,9 @@ from v1pysdk.client import *
 
 class TestV1Connection(TestCase):
   def test_connect(self, username='admin', password='admin'):
-    server = V1Server(username=username, password=password)
-    code, body = server.get('/rest-1.v1/Data/Story')
+    server = V1Server(address='www14.v1host.com', username=username, password=password,instance='v1sdktesting')
+    code, body = server.fetch('/rest-1.v1/Data/Story?sel=Name')
     print "\n\nCode: ", code
     print "Body: ", body
     elem = fromstring(body)
     self.assertThat(elem.tag, Equals('Assets'))
-
-    
-    
-    
-
