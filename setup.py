ba1f38f65f24bf02c7311f25e250ccb1abc3b36d
Aaron McMillin <github@aaron.mcmillinclan.org>
2015-11-24 13:06:55 -0500

    Bump version to post3

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
   name = "v1pysdk-unofficial",
-  version = "0.4.post2",  
-  description = "VersionOne API client",  
+  version = "0.4.post3",
+  description = "VersionOne API client",
   author = "Joe Koberg (VersionOne, Inc.)",
   author_email = "Joe.Koberg@versionone.com",
   license = "MIT/BSD",
   keywords = "versionone v1 api sdk",
   url = "http://github.com/VersionOne/v1pysdk",
-  
+
   packages = [
     'v1pysdk',
-    ],  
+    ],
   include_package_data=True,
   install_requires = [
     'elementtree',
@@ -20,7 +20,7 @@ setup(
     'iso8601',
     'python-ntlm',
     ],
-    
+
   test_suite = "v1pysdk.tests",
-  
+
 )
