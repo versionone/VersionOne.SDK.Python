999c91b134b558d575b250c8611d1c336a436302
kunzimariano <kunzi.mariano@gmail.com>
2016-05-02 17:45:42 -0300

    Added test_build_url.

--- v1pysdk/tests/client_tests.py
+++ v1pysdk/tests/client_tests.py
@@ -3,6 +3,11 @@ from mock import *
 import unittest
 
 class TestClient(unittest.TestCase):
+    address = 'local'
+    instance = 'V1'
+    username = 'usr'
+    password = 'pass'
+    scheme = 'https'
 
     @patch('v1pysdk.client.urllib2.HTTPPasswordMgrWithDefaultRealm')
     @patch('v1pysdk.client.urllib2.build_opener')
@@ -13,10 +18,10 @@ class TestClient(unittest.TestCase):
         build_opener.return_value = self.opener
         HTTPPasswordMgr.return_value = self.manager
 
-        self.server = V1Server()
+        self.server = V1Server(self.address, self.instance, self.username, self.password, self.scheme)
 
     def tearDown(self):
-        print "nothing to do here"
+        print 'nothing to do here'
 
     def test_constructor(self):
         assert self.opener.add_handler.call_count is 1
@@ -27,10 +32,10 @@ class TestClient(unittest.TestCase):
         r = Mock()
         request.return_value = r
 
-        self.server.http_get("http://get.com")
+        self.server.http_get('http://get.com')
 
-        request.assert_called_with("http://get.com")
-        r.add_header.assert_called_with("Content-Type", "text/xml;charset=UTF-8")
+        request.assert_called_with('http://get.com')
+        r.add_header.assert_called_with('Content-Type', 'text/xml;charset=UTF-8')
         self.opener.open.assert_called_with(r)
 
     @patch('v1pysdk.client.Request')
@@ -39,13 +44,17 @@ class TestClient(unittest.TestCase):
         request.return_value = r
         data = Mock()
 
-        self.server.http_post("http://post.com", data)
+        self.server.http_post('http://post.com', data)
 
-        request.assert_called_with("http://post.com", data)
-        r.add_header.assert_called_with("Content-Type", "text/xml;charset=UTF-8")
+        request.assert_called_with('http://post.com', data)
+        r.add_header.assert_called_with('Content-Type', 'text/xml;charset=UTF-8')
         self.opener.open.assert_called_with(r)
 
-    if __name__ == "__main__":
+    def test_build_url(self):
+        url = self.server.build_url('mypath', {'a': 'b'}, 'myfragment')
+        self.assertEqual('https://local/V1/mypath?a=b#myfragment', url)
+
+    if __name__ == '__main__':
         unittest.main()
 
 
